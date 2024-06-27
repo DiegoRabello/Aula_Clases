8e1e0103f7b09549ee2cf720ff70de6edeeece8b
Diego R R Silva <165964850+Decksx7@users.noreply.github.com>
2024-06-27 20:55:55 -0300

    first commit

---
+++ introducao_poo/src/main/java/com/poo_classes/introducao_poo/IntroducaoPooApplication.java
@@ -0,0 +1,15 @@
+package com.poo_classes.introducao_poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoPooApplication.class, args);
+
+		
+	}
+
+}
---
+++ introducao_poo/src/test/java/com/poo_classes/introducao_poo/IntroducaoPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poo_classes.introducao_poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroducaoPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
